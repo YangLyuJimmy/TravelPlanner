d76055b4338f9b3b5aef4695d49984bb1477b460
YangLyuJimmy <jimmylyuyang@outlook.com>
2022-05-01 19:50:12 -0400

    Initial commit

---
+++ src/main/java/com/laioffer/travelplanner/TravelPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.laioffer.travelplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelPlannerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TravelPlannerApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/laioffer/travelplanner/TravelPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laioffer.travelplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelPlannerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
