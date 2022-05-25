e32cce0a3c4b08ed1e714fc84c2dd2542b810326
GiornoPalvin <gryang233@gmail.com>
2022-05-25 19:18:39 -0400

    Update Search RestAPI.

--- src/main/java/com/laioffer/travelplanner/controller/SearchController.java
+++ src/main/java/com/laioffer/travelplanner/controller/SearchController.java
@@ -35,28 +35,8 @@ public class SearchController {
 
     // when frontend call http://localhost:8080/search/category/{category_name}, we return points under that category
     @GetMapping(value = "/search/category")
-    public List<Point> getPoints(@RequestParam(value = "category_name", required = true) String categoryName) {
+    public List<Point> getCategory(@RequestParam(value = "category_name", required = true) String categoryName) {
         return searchService.getPointsbyCate(categoryName);
-    }
-
-  // when frontend call http://localhost:8080/search without parameters, we return all the points
-    // when with parameter "category", input category name, we return all points under that category
-    // when with parameter "category_list", input "TRUE"), we return the complete category list.
-    @GetMapping(value = "/search")
-    public void getPoints(@RequestParam(value ="category", required = false) String category,
-                          @RequestParam(value = "category_list", required = false) String getCateList,
-                          HttpServletResponse response) throws IOException {
-
-        response.setContentType("application/json;charset=UTF-8");
-
-        if(category != null ){
-            response.getWriter().print(new ObjectMapper().writeValueAsString(searchService.getPointsbyCate(category)));
-        } else if (getCateList != null && getCateList.equals("TRUE")) {
-            response.getWriter().print(new ObjectMapper().writeValueAsString(searchService.getCategoryList()));
-        } else {
-            response.getWriter().print(new ObjectMapper().writeValueAsString(searchService.getAllPoints()));
-        }
-
     }
 }
 
